2516abbaa4c5bc993acee45ba41970462bb751c0
1eedaegon <d8726243@gmail.com>
2021-05-07 18:35:16 +0900

    feat: Add create restaurant / Add get restaurants
    GraphQL기반 CR작성 Restaurant entity와 createRestaurant dto를 통해
    그래프 큐엘 리졸버가 작동하는지 테스트

---
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -0,0 +1,23 @@
+import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
+
+@ArgsType()
+export class CreateRestaurantDto {
+  @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
+  name: string;
+
+  @Field((type) => Boolean)
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
+  address: string;
+
+  @Field((type) => String)
+  @IsString()
+  ownersName: string;
+}
